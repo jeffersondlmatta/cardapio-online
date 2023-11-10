5fc2ce9dd134b119d4cc1892730fd0fd51cd8d85
Jefferson da Mata <100040285+jeffersondlmatta@users.noreply.github.com>
2023-11-09 23:04:25 -0300

    v101

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ $(document).ready(function() {
 
 var cardapio = {};
 
+var MEU_CARRINHO = [];
+
 cardapio.eventos = {
 
     init: () => { // essa e uma função e pode ter qualquer nome 
@@ -35,6 +37,7 @@ cardapio.metodos = {
             let temp = cardapio.templates.item.replace(/\${img}/g, e.img)
             .replace(/\${nome}/g, e.name)
             .replace(/\${preco}/g, e.price.toFixed(2).replace('.', (',')))
+            .replace(/\${id}/g, e.id)
 
             // BTN VER MAIS clicado
             if (vermais && i >= 8 && i < 12 ) {
@@ -66,13 +69,72 @@ cardapio.metodos = {
         $("#btnVerMais").addClass('hidden');
 
     },
+    //BTN (-)
+    diminuirQuantidade: (id) => {
+
+        let qntdAtual = parseInt( $('#qntd-' + id).text());
+
+        if (qntdAtual > 0 ) {
+            $('#qntd-' + id).text(qntdAtual - 1)
+        }
+
+    },
+    //BTN (+)
+    aumentarQuantidade: (id) => {
+
+        let qntdAtual = parseInt( $('#qntd-' + id).text());
+        $('#qntd-' + id).text(qntdAtual +  1)
+        
+    },
+    //adcionar item ao carrinho 
+    adcionarAoCarrinho: (id) => {
+
+        let qntdAtual = parseInt( $('#qntd-' + id).text());
+
+        if (qntdAtual > 0 ) {
+            //obter catg. Ativa 
+            var categoria =  $('.container-menu a.active').attr('id').split('menu-')[1];
+            //obtem a lista de itens - filtro dos itens 
+            let filtro = MENU[categoria];
+            // obtem o iten 
+            let item = $.grep(filtro, (e,i) => { return e.id == id});
+
+            if (item.length > 0) {
+
+                // validar se ja existe o item no carrinho 
+
+                let existe = $.grep(MEU_CARRINHO, (elem,index) => { return elem.id == id});
+                // se eciste no carrinho altera a qntd
+                if(existe.length > 0) {
+                    let objIndex = MEU_CARRINHO.findIndex((obj => obj.id == id));
+                    MEU_CARRINHO[objIndex].qntd = MEU_CARRINHO[objIndex].qntd + qntdAtual;
+                }
+                //se nao, adciona o mesmo 
+                else {
+
+                }
+
+                item[0].qntd = qntdAtual;
+                MEU_CARRINHO.push(item[0])
+            }
+
+            $('#qntd-' + id ).text(0);
+
+
+
+
+        }
+
+
+
+    }
 
 }
 
 cardapio.templates = {
 
     item: `<div class="col-3 mb-5">
-    <div class="card card-item">
+    <div class="card card-item" id="\${id}">
         <div class="img-produto">
             <img src="\${img}" >
         </div>  
@@ -83,10 +145,10 @@ cardapio.templates = {
             <b>R$ \${preco}</b>
         </p>
         <div class="add-carrinho">
-            <span class="btn-menos"><i class="fas fa-minus"></i></span>
-            <span class="add-numero-itens">0</span>
-            <span class="btn-mais"><i class="fas fa-plus"></i></span>
-            <span class="btn btn-add"><i class="fa fa-shopping-bag"></i></span>
+            <span class="btn-menos" onclick="cardapio.metodos.diminuirQuantidade('\${id}')"><i class="fas fa-minus"></i></span>
+            <span class="add-numero-itens" id="qntd-\${id}">0</span>
+            <span class="btn-mais" onclick="cardapio.metodos.aumentarQuantidade('\${id}')"><i class="fas fa-plus"></i></span>
+            <span class="btn btn-add" onclick="cardapio.metodos.adcionarAoCarrinho('\${id}')" ><i class="fa fa-shopping-bag"></i></span>
         </div>
     </div>
 </div>
